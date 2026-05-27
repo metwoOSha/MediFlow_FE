08443cca5ad3f358cff08fff7ed6a9d547b3d957
Dmytro Dobrovolskyi <dimadobrovolski@gmail.com>
2026-05-27 18:08:54 +0300

    initial project

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+const eslintConfig = defineConfig([
+    ...nextVitals,
+    ...nextTs,
+    eslintConfigPrettier,
+    globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
+    {
+        rules: {
+            '@typescript-eslint/no-unused-vars': 'off',
+            'no-console': 'warn',
+            'prefer-const': 'error',
+            'react/self-closing-comp': 'error',
+            '@typescript-eslint/no-explicit-any': 'error',
+            '@typescript-eslint/consistent-type-imports': 'error',
+        },
+    },
+]);
+
+export default eslintConfig;
